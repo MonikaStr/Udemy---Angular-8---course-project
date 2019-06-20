29c46576772749c12088149390d072e58e8b1839
Monika Mazur <monika.mazurstr@gmail.com>
2019-06-20 08:08:27 +0200

    code including lecture 51

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Test description', 'https://showmetheyummy.com/easy-summer-recipes-2018/')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
