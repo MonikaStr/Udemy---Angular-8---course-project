1e2fff980caa83acb7ea487ac3f16989a1397efe
Monika Mazur <monika.mazurstr@gmail.com>
2019-06-20 14:27:39 +0200

    after 57 lecture

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'Test description', 'https://kolorowyprzepisownik.pl/wp-content/uploads/2017/12/Najlepsze-przepisy-na-Sylwestra_imprez%C4%99.jpg'),
+    new Recipe('Tomato Soup', 'For a quick lunch', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaAqwmgdfK-OEQ8SQi9j0CGzNsEQemYwijzKqpCIlZj9No6xcE'),
     new Recipe('Pancakes', 'The best for breakfast', 'https://www.kwestiasmaku.com/sites/kwestiasmaku.com/files/nalesniki_cienkie.jpg')
   ];
 
